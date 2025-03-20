a5d4b0c7c805ab7767ba9b7f47b1090daadeb1ab
Loke <lokeinbox@gmail.com>
2025-03-20 08:37:18 +0100

    feat: remove unused icon entries from manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -15,16 +15,6 @@ export default function manifest(): MetadataRoute.Manifest {
         sizes: 'any',
         type: 'image/x-icon',
       },
-      {
-        src: '/icon-192x192.png',
-        sizes: '192x192',
-        type: 'image/png',
-      },
-      {
-        src: '/icon-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
-      },
     ],
   }
 }
