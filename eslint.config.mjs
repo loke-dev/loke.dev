a346c5420cc349052ba0dd1b9e94fa103b362bfd
Loke <lokeinbox@gmail.com>
2025-03-22 20:10:16 +0100

    chore: update configuration files and dependencies for ESLint and Prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,47 +1,19 @@
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
 import { FlatCompat } from '@eslint/eslintrc'
-import tseslint from '@typescript-eslint/eslint-plugin'
-import tsparser from '@typescript-eslint/parser'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: import.meta.dirname,
+  recommendedConfig: {
+    rules: {
+      'react/no-unescaped-entities': 'off',
+    },
+  },
 })
 
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
-  ...compat.extends('prettier'),
-  {
-    files: ['**/*.ts', '**/*.tsx'],
-    languageOptions: {
-      parser: tsparser,
-      parserOptions: {
-        project: './tsconfig.json',
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-      },
-    },
-    rules: {
-      '@typescript-eslint/no-unused-vars': [
-        'error',
-        { argsIgnorePattern: '^_' },
-      ],
-      '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
-      '@typescript-eslint/no-non-null-assertion': 'warn',
-      'react-hooks/exhaustive-deps': 'error',
-    },
-  },
-  {
-    rules: {
-      'no-console': ['warn', { allow: ['warn', 'error'] }],
-      'no-unused-vars': 'off',
-    },
-  },
+  ...compat.config({
+    extends: ['next', 'next/typescript', 'eslint:recommended'],
+    rules: {},
+  }),
 ]
 
 export default eslintConfig
