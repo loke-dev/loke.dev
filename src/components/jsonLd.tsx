a76eea8d4ed69b8bbb06d6426908fa42fdede2dd
Loke <lokeinbox@gmail.com>
2025-03-29 17:56:13 +0100

    fix: wrong file casings

---
+++ src/components/jsonLd.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+interface JsonLdProps {
+  data: Record<string, unknown>
+}
+
+export default function JsonLd({ data }: JsonLdProps) {
+  return (
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
+    />
+  )
+}
