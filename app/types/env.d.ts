88faf237e9661ace255c1538d0d8a21e278161d3
Loke <lokeinbox@gmail.com>
2026-01-21 23:46:00 +0100

    feat: remove outdated cache invalidation guide and related TypeScript definitions

--- app/types/env.d.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/// <reference types="@remix-run/node" />
-/// <reference types="vite/client" />
-
-declare module '*.mdx' {
-  let MDXComponent: (props: any) => JSX.Element
-  export const frontmatter: any
-  export default MDXComponent
-}
